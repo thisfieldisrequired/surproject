a5a57965fcb737d3d092c868dd3ec1eb6426c015
sine <sudodebug@gmail.com>
2024-03-02 08:18:25 +0500

    create @asynccontextmanager lifespan

--- core/models/__init__.py
+++ core/models/__init__.py
@@ -1,7 +1,10 @@
 __all__ = (
     "Base",
     "Product",
+    "DataBaseHelper",
+    "db_helper",
 )
 
 from .base import Base
 from .product import Product
+from .db_helper import DataBaseHelper, db_helper
--- main.py
+++ main.py
@@ -1,4 +1,18 @@
 from fastapi import FastAPI
+from contextlib import asynccontextmanager
+from core.models import Base, db_helper
 
-app = FastAPI()
 
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    async with db_helper.engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
+    yield
+
+
+app = FastAPI(lifespan=lifespan)
+
+
+@app.get("/")
+async def index():
+    return "Hello World!"
