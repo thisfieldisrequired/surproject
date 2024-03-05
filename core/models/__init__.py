34aa06f8df64975b8b6c96a495cc795496dfefe3
sine <sudodebug@gmail.com>
2024-03-05 08:29:02 +0500

    create posts model

--- core/models/__init__.py
+++ core/models/__init__.py
@@ -1,6 +1,7 @@
 __all__ = (
     "Base",
     "User",
+    "Post",
     "Product",
     "DataBaseHelper",
     "db_helper",
@@ -8,5 +9,6 @@ __all__ = (
 
 from .base import Base
 from .user import User
+from .post import Post
 from .product import Product
 from .db_helper import DataBaseHelper, db_helper
