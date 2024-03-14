70d1df8d2bb5beceefc9b3be0551d087ada8fb30
sine <sudodebug@gmail.com>
2024-03-14 15:48:56 +0500

    create UserSchema in users/schemas.py

--- users/schemas.py
+++ users/schemas.py
@@ -1,8 +1,17 @@
 from typing import Annotated
 from annotated_types import MaxLen, MinLen
-from pydantic import BaseModel, EmailStr
+from pydantic import BaseModel, EmailStr, ConfigDict
 
 
 class CreateUser(BaseModel):
     username: Annotated[str, MinLen(2), MaxLen(10)]
     email: EmailStr
+
+
+class UserSchema(BaseModel):
+    model_config = ConfigDict(strict=True)
+
+    username: str
+    password: bytes
+    email: EmailStr | None = None
+    active: bool = True
