d4a789f47d0439a126b222efd701bd6aaf1bf7d1
sine <sudodebug@gmail.com>
2024-03-14 08:25:02 +0500

    install pyjwt, create README.md in auth

--- api_v1/__init__.py
+++ api_v1/__init__.py
@@ -1,9 +1,14 @@
 from fastapi import APIRouter
 
 from .products.views import router as products_router
+from .demo_auth.views import router as basic_auth_router
 
 router = APIRouter()
 router.include_router(
     router=products_router,
     prefix="/products",
+)
+
+router.include_router(
+    router=basic_auth_router,
 )
