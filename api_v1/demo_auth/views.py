d15288e8c77bbfc7121d46e1c2533872e858d73e
sine <sudodebug@gmail.com>
2024-03-14 08:24:19 +0500

    create base auth, token auth, cookie auth

--- api_v1/demo_auth/views.py
+++ api_v1/demo_auth/views.py
@@ -1,3 +1,144 @@
-from fastapi import APIRouter
+import secrets
+import uuid
+from time import time
+
+from typing import Annotated, Any
+
+from fastapi import APIRouter, Depends, HTTPException, status, Header, Response, Cookie
+from fastapi.security import HTTPBasic, HTTPBasicCredentials
 
 router = APIRouter(prefix="/demo_auth", tags=["Demo Auth"])
+
+
+security = HTTPBasic()
+
+
+@router.get("/basic-auth/")
+async def demo_basic_auth_credentials(
+    credentials: Annotated[HTTPBasicCredentials, Depends(security)]
+):
+    return {
+        "message": "Hi!",
+        "credentials": credentials.username,
+        "password": credentials.password,
+    }
+
+
+user_to_password = {
+    "admin": "admin",
+    "john": "password",
+}
+
+
+static_auth_token_to_username = {
+    "adfgdfsgesrgsdfgsdg": "admin",
+    "hjkhjklhjklhjklhjkl": "john",
+}
+
+
+def get_username_by_static_auth_token(
+    static_auth_token: str = Header(alias="x-auth-token"),
+) -> str:
+    if username := static_auth_token_to_username.get(static_auth_token):
+        return username
+    raise HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Invalid token",
+    )
+
+
+async def get_auth_user_username(
+    credentials: Annotated[HTTPBasicCredentials, Depends(security)]
+):
+    unauthed_exc = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Invalid username or password",
+        headers={"WWW-Authenticate": "Basic"},
+    )
+    correct_password = user_to_password.get(credentials.username)
+    if correct_password is None:
+        raise unauthed_exc
+    if not secrets.compare_digest(
+        credentials.password.encode("utf-8"),
+        correct_password.encode("utf-8"),
+    ):
+        return unauthed_exc
+    return credentials.username
+
+
+@router.get("/basic-auth-username/")
+async def demo_basic_auth_username(
+    auth_username: str = Depends(get_auth_user_username),
+):
+    return {
+        "message": f"Hi {auth_username}",
+        "username": auth_username,
+    }
+
+
+@router.get("/some-http-header-auth/")
+async def demo_auth_some_http_header(
+    username: str = Depends(get_username_by_static_auth_token),
+):
+    return {
+        "message": f"Hi {username}",
+        "username": username,
+    }
+
+
+COOKIES: dict[str, dict[str, Any]] = {}
+COOKIE_SESSION_ID_KEY = "web-app-session-id"
+
+
+def generate_session_id() -> str:
+    return uuid.uuid4().hex
+
+
+def get_session_data(
+    session_id: str = Cookie(alias=COOKIE_SESSION_ID_KEY),
+) -> dict:
+    if session_id not in COOKIES:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED, detail="Not authenticated"
+        )
+    return COOKIES[session_id]
+
+
+@router.post("/login-cookie/")
+async def demo_auth_login_set_cookie(
+    response: Response,
+    # auth_username: str = Depends(get_auth_user_username),
+    username: str = Depends(get_username_by_static_auth_token),
+):
+    session_id = generate_session_id()
+    COOKIES[session_id] = {
+        "username": username,
+        "login_at": int(time()),
+    }
+    print(f"COOKIES ------- {COOKIES}")
+    response.set_cookie(COOKIE_SESSION_ID_KEY, session_id)
+    return {"result": "ok"}
+
+
+@router.post("/check-cookie/")
+async def demo_auth_check_cookie(
+    user_session_data: dict = Depends(get_session_data),
+):
+    username = user_session_data["username"]
+    print(user_session_data)
+    return {
+        "message": f"Hello, {username}!",
+        **user_session_data,
+    }
+
+
+@router.get("/logout-cookie/")
+async def demo_auth_logout_cookie(
+    response: Response,
+    session_id: str = Cookie(alias=COOKIE_SESSION_ID_KEY),
+    user_session_data=Depends(get_session_data),
+):
+    COOKIES.pop(session_id)
+    response.delete_cookie(COOKIE_SESSION_ID_KEY)
+    username = user_session_data["username"]
+    return {"message": f"Bye, {username}"}
